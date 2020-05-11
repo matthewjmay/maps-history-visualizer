be87b612b651c47a92364447f5850ee9d6941602
matthewjmay <matthewmay@fb.com>
2020-05-10 22:35:10 -0400

    indexed db caching

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { openDB, deleteDB, wrap, unwrap } from "idb";
+
 import {
   chunkFileBlocking,
   getLatLng,
@@ -19,9 +21,100 @@ class App extends React.Component {
     latLngs: {},
     historySettings: {},
     nextId: 0,
+    doneLoading: false,
+  };
+
+  db = null;
+
+  async componentDidMount() {
+    if (!("indexedDB" in window)) {
+      console.log(
+        "Browser does not support IndexedDB, no caching will be done"
+      );
+      return;
+    }
+    this.db = openDB("maps-history-visualizer", 3, {
+      upgrade: (db) => {
+        if (!db.objectStoreNames.contains("historyIds")) {
+          db.createObjectStore("historyIds");
+        }
+        if (!db.objectStoreNames.contains("historySettings")) {
+          db.createObjectStore("historySettings");
+        }
+        if (!db.objectStoreNames.contains("latLngs")) {
+          db.createObjectStore("latLngs");
+        }
+      },
+    });
+    const db = await this.db;
+    const tx = db.transaction(
+      ["historyIds", "historySettings", "latLngs"],
+      "readonly"
+    );
+
+    const idStore = tx.objectStore("historyIds");
+    const ids = await idStore.getAll();
+    if (ids.length === 0) return;
+    this.setState({ nextId: Math.max(...ids) + 1 });
+
+    const settingsStore = tx.objectStore("historySettings");
+    const latLngsStore = tx.objectStore("latLngs");
+    // start concurrent fetches
+    const idData = ids.map((id) => ({
+      id,
+      settingsPromise: settingsStore.get(id),
+      latLngsPromise: latLngsStore.get(id),
+    }));
+
+    await Promise.all(
+      idData.map(async ({ id, settingsPromise, latLngsPromise }) => {
+        const settings = await settingsPromise;
+        const latLngs = await latLngsPromise;
+        this.setState((prevState) => ({
+          historyIds: [...prevState.historyIds, id],
+          historySettings: {
+            ...prevState.historySettings,
+            [id]: settings,
+          },
+          latLngs: {
+            ...prevState.latLngs,
+            [id]: latLngs,
+          },
+        }));
+      })
+    );
+    this.setState({ doneLoading: true });
+  }
+
+  cacheNewHistory = async (historyId) => {
+    const db = await this.db;
+    const tx = db.transaction(
+      ["historyIds", "historySettings", "latLngs"],
+      "readwrite"
+    );
+    tx.objectStore("historyIds").add(historyId, historyId);
+    tx.objectStore("historySettings").add(
+      this.state.historySettings[historyId],
+      historyId
+    );
+    tx.objectStore("latLngs").add(this.state.latLngs[historyId], historyId);
+    await tx.complete;
+    console.log(`Saved new history ${historyId} to database`);
+  };
+
+  cacheSettings = async (historyId) => {
+    const db = await this.db;
+    const tx = db.transaction("historySettings", "readwrite");
+    tx.objectStore("historySettings").put(
+      this.state.historySettings[historyId],
+      historyId
+    );
+    await tx.complete;
+    console.log(`Saved history settings for ${historyId} to database`);
   };
 
   handleStartUploadNewHistory = async (file) => {
+    if (!this.state.doneLoading) return;
     console.log(`Uploading new file: ${file.name}`);
 
     // update UI to show we started upload
@@ -66,50 +159,65 @@ class App extends React.Component {
 
   handleHistoryLabelChanged = (label, historyId) => {
     console.log(`Changing id ${historyId} to label ${label}`);
-    this.setState((prevState, props) => ({
-      historySettings: {
-        ...prevState.historySettings,
-        [historyId]: {
-          ...prevState.historySettings[historyId],
-          label,
-        },
-      },
-    }));
-  };
-
-  handleToggleHistoryShownOnMap = (historyId) => {
-    this.setState((prevState, props) => {
-      console.log(
-        `Changing id ${historyId} to ${
-          !prevState.historySettings[historyId].isShownOnMap
-            ? "visible"
-            : "hidden"
-        }`
-      );
-
-      return {
+    this.setState(
+      (prevState, props) => ({
         historySettings: {
           ...prevState.historySettings,
           [historyId]: {
             ...prevState.historySettings[historyId],
-            isShownOnMap: !prevState.historySettings[historyId].isShownOnMap,
+            label,
           },
         },
-      };
-    });
+      }),
+      () => {
+        this.cacheSettings(historyId);
+      }
+    );
+  };
+
+  handleToggleHistoryShownOnMap = (historyId) => {
+    this.setState(
+      (prevState, props) => {
+        console.log(
+          `Changing id ${historyId} to ${
+            !prevState.historySettings[historyId].isShownOnMap
+              ? "visible"
+              : "hidden"
+          }`
+        );
+
+        return {
+          historySettings: {
+            ...prevState.historySettings,
+            [historyId]: {
+              ...prevState.historySettings[historyId],
+              isShownOnMap: !prevState.historySettings[historyId].isShownOnMap,
+            },
+          },
+        };
+      },
+      () => {
+        this.cacheSettings(historyId);
+      }
+    );
   };
 
   handleHistoryColorChanged = (color, historyId) => {
     console.log(`Changing id ${historyId} to color ${color}`);
-    this.setState((prevState, props) => ({
-      historySettings: {
-        ...prevState.historySettings,
-        [historyId]: {
-          ...prevState.historySettings[historyId],
-          color,
+    this.setState(
+      (prevState, props) => ({
+        historySettings: {
+          ...prevState.historySettings,
+          [historyId]: {
+            ...prevState.historySettings[historyId],
+            color,
+          },
         },
-      },
-    }));
+      }),
+      () => {
+        this.cacheSettings(historyId);
+      }
+    );
   };
 
   handleDoneUploadNewHistory = (latLngs, historyId) => {
@@ -129,16 +237,19 @@ class App extends React.Component {
         },
       }),
       () => {
-        this.setState((prevState, props) => ({
-          historySettings: {
-            ...prevState.historySettings,
-            [historyId]: {
-              ...prevState.historySettings[historyId],
-              loadingProgress: 1,
-              isFirstShow: false,
+        this.setState(
+          (prevState, props) => ({
+            historySettings: {
+              ...prevState.historySettings,
+              [historyId]: {
+                ...prevState.historySettings[historyId],
+                loadingProgress: 1,
+                isFirstShow: false,
+              },
             },
-          },
-        }));
+          }),
+          () => this.cacheNewHistory(historyId)
+        );
       }
     );
   };
